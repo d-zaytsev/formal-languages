87ce4316f811b309c324433f28bc069c7330ae3a
d-zaytsev <dzaytsev308@gmail.com>
2025-01-26 13:51:41 +0300

    write regex interpret

--- project/interpreter/runner/regex.py
+++ project/interpreter/runner/regex.py
@@ -1,144 +1,119 @@
 from pyformlang.regular_expression import Regex
-from pyformlang.finite_automaton import EpsilonNFA
+from pyformlang.finite_automaton import EpsilonNFA, Epsilon, State
 from typing import Optional
 
 
-class RunnableRegex:
-    def __init__(self):
-        self.__regex = ""
-        self.__is_created = False
-
-    __UNIQUE_VAR_PREFIX = "VAR_"
-
-    @property
-    def str(self) -> str:
-        assert self.__is_created, "Create RunnableRegex first."
-        return self.__regex
-
-    @property
-    def nfa(self) -> EpsilonNFA:
-        assert self.__is_created, "Create RunnableRegex first."
-        return Regex(self.__regex).to_epsilon_nfa()
-
-    def init_with_char(self, char: str):
-        assert not self.__is_created, "RunnableRegex already has been created."
-        self.__is_created = True
-
-        self.__regex = char
-
-        return self
-
-    def init_with_variable(self, var_name: str):
-        assert not self.__is_created, "RunnableRegex already has been created."
-        self.__is_created = True
-
-        new_name = self.__transform_var_name(var_name)
-
-        self.__regex = new_name
-
-        return self
-
-    def init_with_regex(self, regex: Regex):
-        assert not self.__is_created, "RunnableRegex already has been created."
-        self.__is_created = True
-
-        self.__regex = str(regex)
-
-        return self
-
-    def __transform_var_name(self, var_name: str):
-        """Return variable name for internal usage."""
-
-        return self.__UNIQUE_VAR_PREFIX + var_name
-
-    def __get_common_var_name(self, internal_var_name: str):
-        return internal_var_name.replace(self.__UNIQUE_VAR_PREFIX, "")
+UNIQUE_VARIABLE_PREFFIX = "VAR_"
 
 
-def intersect(regexA: RunnableRegex, regexB: RunnableRegex) -> RunnableRegex:
-    """regexA & regexB"""
-    nfaA = regexA.nfa
-    nfaB = regexB.nfa
-
-    intersected_nfa: EpsilonNFA = nfaA.get_intersection(nfaB).minimize()
-    intersected_regex: Regex = intersected_nfa.to_regex()
-
-    return RunnableRegex().init_with_regex(f"{intersected_regex}")
+def nfa_from_char(char: str) -> EpsilonNFA:
+    assert len(char) == 1, f"Incorrect char: '{char}'."
+    return Regex(char).to_epsilon_nfa()
 
 
-def concatenate(regexA: RunnableRegex, regexB: RunnableRegex) -> RunnableRegex:
-    """regexA . regexB"""
-    nfaA = regexA.nfa
-    nfaB = regexB.nfa
-
-    concatenated_nfa: EpsilonNFA = nfaA.concatenate(nfaB).minimize()
-    concatenated_regex: Regex = concatenated_nfa.to_regex()
-
-    return RunnableRegex().init_with_regex(f"{concatenated_regex}")
+def nfa_from_var(var_name: str) -> EpsilonNFA:
+    return Regex(f"{UNIQUE_VARIABLE_PREFFIX}{var_name}").to_epsilon_nfa()
 
 
-def union(regexA: RunnableRegex, regexB: RunnableRegex) -> RunnableRegex:
-    """regexA | regexB"""
-    nfaA = regexA.nfa
-    nfaB = regexB.nfa
-
-    union_nfa: EpsilonNFA = nfaA.union(nfaB).minimize()
-    union_regex: Regex = union_nfa.to_regex()
-
-    return RunnableRegex().init_with_regex(f"{union_regex}")
+def create_empty_nfa() -> EpsilonNFA:
+    return Regex("$").to_epsilon_nfa()
 
 
-def repeat(regex: RunnableRegex, times: int) -> RunnableRegex:
+def intersect(nfa1: EpsilonNFA, nfa2: EpsilonNFA) -> EpsilonNFA:
+    """regex & regex"""
+    intersected_nfa: EpsilonNFA = nfa1.get_intersection(nfa2)
+
+    return intersected_nfa.minimize()
+
+
+def concatenate(nfa1: EpsilonNFA, nfa2: EpsilonNFA) -> EpsilonNFA:
+    """regex . regex"""
+    concatenated_nfa: EpsilonNFA = nfa1.concatenate(nfa2)
+
+    return concatenated_nfa.minimize()
+
+
+def union(nfa1: EpsilonNFA, nfa2: EpsilonNFA) -> EpsilonNFA:
+    """regex | regex"""
+    union_nfa: EpsilonNFA = nfa1.union(nfa2)
+
+    return union_nfa.minimize()
+
+
+def repeat(nfa: EpsilonNFA, times: int) -> EpsilonNFA:
     """regex ^ [times]"""
     if times == 0:
-        return RunnableRegex().init_with_regex("")
+        return create_empty_nfa()
 
-    result = regex
+    repeat_nfa = nfa
 
     for _ in range(times - 1):
-        result = concatenate(result, regex)
+        repeat_nfa = concatenate(repeat_nfa, nfa)
 
-    return result
+    return repeat_nfa.minimize()
 
-def klini(regex: RunnableRegex):
-    return RunnableRegex().init_with_regex(f"{regex.str}*")
 
-def sequence(regex: RunnableRegex, left_border: int, right_border: Optional[int]) -> RunnableRegex:
-    """regex ^ [left_border...right_border]"""
-    regex_str = regex.str
+def kleene(nfa: EpsilonNFA) -> EpsilonNFA:
+    """regex *"""
+    return nfa.kleene_star().minimize()
 
-    if left_border == 0 and right_border is None: # ^ [0..]
-        return RunnableRegex().init_with_regex(f"({regex_str}*)")
 
-    if right_border is None: # ^ [left_border..]
+def repeat_range(
+    nfa: EpsilonNFA, left_border: int, right_border: Optional[int]
+) -> EpsilonNFA:
+    """regex ^ [left_border..right_border]"""
+    if left_border == 0 and right_border is None:  # ^ [0..]
+        return kleene(nfa)
+
+    if right_border is None:  # ^ [left_border..]
         # "a" ^ [2..] => "a"."a".("a"*)
-        repeated_regex = repeat(regex, left_border)
-        concat_regex = concatenate(repeated_regex, klini(regex))
+        repeated_regex = repeat(nfa, left_border)
+        concat_regex = concatenate(repeated_regex, kleene(nfa))
 
         return concat_regex
 
-    result_regex = RunnableRegex().init_with_regex('')
+    if left_border == right_border:  # ^ [x..x]
+        return repeat(nfa, left_border)
 
-    for times in range(left_border, right_border + 1):
-        result_regex = union(result_regex, repeat(regex, times))
+    result_nfa = repeat(nfa, left_border)
 
-    return result_regex
+    for times in range(left_border + 1, right_border + 1):
+        repeat_res = repeat(nfa, times)
+        result_nfa = union(result_nfa, repeat_res)
+
+    return result_nfa.minimize()
 
 
-def group(regex: RunnableRegex):
+def group(nfa: EpsilonNFA) -> EpsilonNFA:
     """( regex )"""
-    return RunnableRegex().init_with_regex(f"({regex.str})")
+    return EpsilonNFA(Regex(f"({nfa.minimize().to_regex()})"))
+
+def subs_vars(var_nfa: EpsilonNFA, subs_dict: dict[str, EpsilonNFA]) -> EpsilonNFA:
+    str_regex = str(var_nfa.minimize().to_regex())
+
+    for var_name, var_nfa in subs_dict.items():
+        prefixed_var_name = f"{UNIQUE_VARIABLE_PREFFIX}{var_name}"
+        var_nfa_str_regex = f'({var_nfa.minimize().to_regex()})'
+        
+        str_regex = str_regex.replace(prefixed_var_name, var_nfa_str_regex)
+
+    return Regex(str_regex).to_epsilon_nfa()
 
 
-# let q = (("a" | "b") . ("b" | "c")) ^ [0..]
+# # "a"^[1..3] . q . "b"^[2..3] | "c"
 
-a = RunnableRegex().init_with_char("a")
-b = RunnableRegex().init_with_char("b")
-c = RunnableRegex().init_with_char("c")
+# a = nfa_from_char("a")
+# b = nfa_from_char("b")
+# c = nfa_from_char("c")
+# var_q = nfa_from_var("q")
 
-a_or_b = union(a, b)
-b_or_c = union(b, c)
+# repeat_a = repeat_range(a, 1, 3)
+# repeat_b = repeat_range(b, 2, 3)
+# big_expr = concatenate(concatenate(repeat_a, var_q), repeat_b)
+# full_expr = union(big_expr, c)
 
-full_reg = concatenate(a_or_b, b_or_c)
+# subs_dict = {'q': nfa_from_char("d")}
+# #"a"^[1..3] . "d" . "b"^[2..3] | "c"
+# # => "adbbb", "c", "aadbbb"
 
-print(sequence(a_or_b, 0, 3).nfa.accepts(''))
+# print(subs_vars(full_expr, subs_dict).accepts('aadbbbb'))
