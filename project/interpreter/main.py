3ed069a6f8a1e54dd7f4e27d500e142b957b3c98
d-zaytsev <dzaytsev308@gmail.com>
2025-01-27 21:21:13 +0300

    uncomment main

--- project/interpreter/main.py
+++ project/interpreter/main.py
@@ -108,16 +108,16 @@ if __name__ == "__main__":
         except Exception as ex:
             typecheck_result = str(ex)
 
-        # try:
-        runner_visitor.visit(tree)
-        # except Exception as ex:
-        # runner_error_msg = str(ex)
+        try:
+            runner_visitor.visit(tree)
+        except Exception as ex:
+            runner_error_msg = str(ex)
 
-        # print("Length:", nodes_count(tree))
-        # print("Recovered text:", tree_to_program(tree))
+        print("Length:", nodes_count(tree))
+        print("Recovered text:", tree_to_program(tree))
 
         print("---")
-        # print("Parser:", tree.toStringTree(recog=parser))
+        print("Parser:", tree.toStringTree(recog=parser))
 
         print("Typecheker:", typecheck_result if typecheck_result else "")
         if not typecheck_result:
